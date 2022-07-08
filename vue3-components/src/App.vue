49de8d68f340cf57ecb9ee8a947836d2c25c54c9
seokyHan <sseok1979@naver.com>
2022-07-08 17:54:42 +0900

    practice vue3

---
+++ vue3-components/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- 해당 component에 slot으로 전달 -->
+  <MyBtn>Banana</MyBtn>
+  <MyBtn :color="color">Apple</MyBtn>
+  <MyBtn large color="royalblue" >Cherry</MyBtn>
+  <MyBtn >strawberry</MyBtn>
+</template>
+
+<script>
+import MyBtn from '~/components/MyBtn'
+
+export default {
+  components: {
+    MyBtn
+  },
+  data() {
+    return {
+      color: '#000'
+    }
+  }
+}
+</script>
