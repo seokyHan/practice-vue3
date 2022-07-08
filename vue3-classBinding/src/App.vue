7607f61a1d9c5e758d1a34a88672cb6d70dd1135
seokyHan <sseok1979@naver.com>
2022-07-08 09:46:00 +0900

    practice vue3

---
+++ vue3-classBinding/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <!-- isActive가 true면 h1 class에 active가 부여되고 false는 X -->
+  <h1
+    :class="{active: isActive}"
+    @click="activate">
+    Hello?({{ isActive }})
+  </h1>
+
+  <h1
+    :style="fontStyle"
+    @click="changeStyle">
+    Hi
+  </h1>
+
+  <h1
+    :style="[fontStyle, backgroundStyle]"
+    @click="changeStyle">
+    Hello
+  </h1>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      isActive: false,
+      fontStyle: {
+        color: 'orange',
+        fontSize: '30px'
+      },
+      backgroundStyle: {
+        backgroundColor: 'black'
+      }
+    }
+  },
+  methods: {
+    activate() {
+      this.isActive = true
+    },
+    changeStyle(){
+      this.fontStyle.color = 'blue'
+      this.fontStyle.fontSize = '50px'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.active{
+    color:royalblue;
+}
+</style>
---
+++ vue3-lifecycle/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>{{ count }}</h1>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      count: 2
+    }
+  },
+  beforeCreate(){
+    // undefined
+    console.log('before Create!', this.count );
+  },
+  created(){
+    // 2
+    console.log('Created!', this.count);
+  },
+  beforeMount(){
+    // null html과 연결이 되기전 이라서
+    console.log('before Mount!');
+    console.log(document.querySelector('h1'));
+  },
+  mounted(){
+    // <h1>2</h1> html과 연결된 후
+    console.log('Mounted!');
+    console.log(document.querySelector('h1'));
+  }
+}
+</script>
