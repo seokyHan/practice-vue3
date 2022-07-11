fa0bbcc569d561303084fd579ebefec661472c1f
seokyHan <sseok1979@naver.com>
2022-07-11 16:50:12 +0900

    vue3-Provid, Inject

---
+++ vue3-Provide-Inject/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <button @click="message = 'Good?'">
+    click!
+  </button>
+  <h1>App: {{ message }}</h1>
+  <Parent />
+</template>
+
+<script>
+import Parent from '~/components/Parent'
+import { computed } from 'vue'
+
+export default {
+  components: {
+    Parent
+  },
+  data() {
+    return {
+      message: 'Hello vue!'
+    }
+  },
+  provide() {
+    return {
+      msg: computed(() => {
+        return this.message
+      })
+    }
+  }
+}
+</script>
+
---
+++ vue3-Provide-Inject/src/components/Child.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    Child : {{ msg.value }}
+  </div>
+</template>
+
+<script>
+export default {
+  inject: ['msg']
+}
+</script>
+
