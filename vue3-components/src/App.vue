bc600f694e1f7f3d427e54a518f8a27806d6a88f
seokyHan <sseok1979@naver.com>
2022-07-11 15:27:12 +0900

    practice-vue3

--- vue3-components/src/App.vue
+++ vue3-components/src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <!-- 해당 component에 slot으로 전달 -->
+  <!-- 해당 component에 slot으로 전달 
+  root Element가 2개이상이면 class와 style로 상속이 안됨 -->
   <MyBtn>Banana</MyBtn>
-  <MyBtn :color="color">Apple</MyBtn>
-  <MyBtn large color="royalblue" >Cherry</MyBtn>
-  <MyBtn >strawberry</MyBtn>
+  <MyBtn :color="color">
+    Apple
+  </MyBtn>
+  <MyBtn
+    large
+    color="royalblue">
+    Cherry
+  </MyBtn>
+  <MyBtn>strawberry</MyBtn>
+
+  <!-- component - emit 
+  내가 원하는 이름으로 설정 해도 됨-->
+  <MyBtn @sseok="log">
+    grapes
+  </MyBtn>
 </template>
 
 <script>
@@ -16,6 +29,11 @@ export default {
   data() {
     return {
       color: '#000'
+    }
+  },
+  methods:{
+    log(){
+      console.log('click graeps')  
     }
   }
 }
--- vue3-components/src/components/MyBtn.vue
+++ vue3-components/src/components/MyBtn.vue
@@ -5,6 +5,12 @@
     class="btn">
     <slot></slot>
   </div>
+
+  <!-- emit, 상위요소가 2개 이상이면 evnet가 어디 걸릴지 모르니까
+  emit 속성을 통해 부모에서 정의해둔 event 걸어줌 -->
+  <h1 @click="$emit('sseok')">
+    ABC
+  </h1>
 </template>
 
 <script>
@@ -18,7 +24,11 @@ export default {
       type: Boolean,
       default: false
     }
-  }
+  },
+  emits: [
+    'sseok'
+  ]
+  
 }
 </script>
 
